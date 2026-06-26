5c5b4d40cd9655bc3eeb72b673d61aeafd8a5d80
splincode <splincode@users.noreply.github.com>
2026-06-26 08:27:02 +0000

    Remove preview for PR 14441 🛬

--- pr-preview/pr-14441/chunk-QSUUHGKY.js
+++
@@ -1,1 +0,0 @@
-function t(s){return s.replace("@import '@taiga-ui/styles/utils';","@import '@taiga-ui/styles/utils.less';")}export{t as a};
--- pr-preview/pr-14441/chunk-SGMXWOX7.js
+++
@@ -1,1 +0,0 @@
-var n={production:!0,ym:87890624};export{n as a};
